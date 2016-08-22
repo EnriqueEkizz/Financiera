6ca511ff1f968d38d1bf649c8b162f37ef9841f7
Usuario <Usuario@AEPIS16>
2016-08-22 16:39:48 -0500

    lucero

--- Financiera.Dominio/Cliente.cs
+++ Financiera.Dominio/Cliente.cs
@@ -10,6 +10,7 @@ namespace Financiera.Dominio
 
         //lucero
         //que es esto 
+        //lucero
         public int CodigoCliente{ get; set; }
 
         public string NombreCliente { get; set; }
